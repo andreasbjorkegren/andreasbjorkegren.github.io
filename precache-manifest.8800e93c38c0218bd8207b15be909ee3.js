fb80c02c61358efaef342cab63df9b02a52cdf45
andreas.bjorkegren <andreas_bjorkegren@hotmail.com>
2020-04-14 21:50:05 +0200

    first commit

---
+++ precache-manifest.8800e93c38c0218bd8207b15be909ee3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b570bb635d14bc6d3def8b77cf6a35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a63f91a4158197d4937a",
+    "url": "/static/css/main.682bad9f.chunk.css"
+  },
+  {
+    "revision": "c6a2fb42b68a7ca315fb",
+    "url": "/static/js/2.34a55a71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.34a55a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a63f91a4158197d4937a",
+    "url": "/static/js/main.93365a04.chunk.js"
+  },
+  {
+    "revision": "b184ef0eef2d929668c8",
+    "url": "/static/js/runtime-main.7eda86da.js"
+  },
+  {
+    "revision": "128e733327679daa12f5bb19ee464e64",
+    "url": "/static/media/zen.128e7333.png"
+  }
+]);
